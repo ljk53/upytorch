235454675e4a4883b60be5ac9791542156b428e9
Jiakai Liu <liujiakai@fb.com>
2020-12-25 11:00:50 -0800

    bloaty on OS X

--- scripts/run_bloaty.sh
+++ scripts/run_bloaty.sh
@@ -4,16 +4,32 @@
 set -eux -o pipefail
 
 ROOT="$( cd "$(dirname "$0")" ; pwd -P)/.."
+UNAME=$(uname -s)
 
 cd "$ROOT"
 
-make clean
 make DEBUG=1
 
 strip build/upytorch -o build/upytorch.strip
+
+if [ "$UNAME" == "Linux" ]; then
 
 bloaty \
   -d sections,compileunits,symbols \
   -n 0 \
   --debug-file build/upytorch \
   build/upytorch.strip | tee bloaty_output.txt
+
+elif [ "$UNAME" == "Darwin" ]; then
+
+dsymutil build/upytorch
+
+bloaty \
+  --csv \
+  --demangle full \
+  -d sections,compileunits,symbols \
+  -n 0 \
+  --debug-file build/upytorch.dSYM/Contents/Resources/DWARF/upytorch \
+  build/upytorch.strip | tee bloaty_output.txt
+
+fi
