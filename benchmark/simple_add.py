30f311ac31bf291a840f33f7610f53ce83a26fba
Jiakai Liu <48700641+ljk53@users.noreply.github.com>
2021-01-04 04:11:25 -0800

    clean up benchmark naming / params (#10)

--- benchmark/simple_add.py
+++ benchmark/simple_add.py
@@ -2,15 +2,14 @@ import torch
 from common import *
 
 
-def add_s1_nograd_outplace():
-    a = torch.ones(1, requires_grad=False)
-    b = torch.ones(1, requires_grad=False)
-    c = a
+def add_s1_outplace():
+    a = torch.ones(1)
+    b = torch.ones(1)
 
     def run(count):
-        nonlocal b, c
+        nonlocal a, b
         for _ in range(count):
-            torch.add(c, b, out=c)
+            torch.add(a, b, out=a)
 
     return run
 
@@ -18,12 +17,11 @@ def add_s1_nograd_outplace():
 def add_s1():
     a = torch.ones(1)
     b = torch.ones(1)
-    c = a
 
     def run(count):
-        nonlocal b, c
+        nonlocal a, b
         for _ in range(count):
-            c = torch.add(c, b)
+            a = torch.add(a, b)
 
     return run
 
@@ -31,24 +29,23 @@ def add_s1():
 def add_s1024():
     a = torch.ones(1024)
     b = torch.ones(1024)
-    c = a
 
     def run(count):
-        nonlocal b, c
+        nonlocal a, b
         for _ in range(count):
-            c = torch.add(c, b)
+            a = torch.add(a, b)
 
     return run
 
 
 if __name__ == "__main__":
     report_header()
-    benchmark("add_s1_nograd_outplace_inc_gc_10000", add_s1_nograd_outplace(), 10000, include_gc=True)
-    benchmark("add_s1_nograd_outplace_inc_gc_100000", add_s1_nograd_outplace(), 100000, include_gc=True)
+    benchmark("add_s1_outplace_N10000", add_s1_outplace(), 10000, include_gc=True)
+    benchmark("add_s1_outplace_N100000", add_s1_outplace(), 100000, include_gc=True)
     print()
-    benchmark("add_s1_inc_gc_10000", add_s1(), 10000, include_gc=True)
-    benchmark("add_s1_inc_gc_100000", add_s1(), 100000, include_gc=True)
+    benchmark("add_s1_N10000", add_s1(), 10000, include_gc=True)
+    benchmark("add_s1_N100000", add_s1(), 100000, include_gc=True)
     print()
-    benchmark("add_s1024_inc_gc_10000", add_s1024(), 10000, include_gc=True)
-    benchmark("add_s1024_inc_gc_100000", add_s1024(), 100000, include_gc=True)
+    benchmark("add_s1024_N10000", add_s1024(), 10000, include_gc=True)
+    benchmark("add_s1024_N100000", add_s1024(), 100000, include_gc=True)
     print()
